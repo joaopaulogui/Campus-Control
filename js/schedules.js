9884f0e524de0375f8c622a99a14c468b77be9ec
joaopaulogui <joaopaulogmormota@gmail.com>
2026-06-21 15:37:32 -0300

    feat: start schedules page

--- js/schedules.js
+++ js/schedules.js
@@ -2,5 +2,20 @@ const allFloors = getFloors()
 let floors = allFloors;
 
 initFloorFilter(allFloors, () => {
-
+    renderSchedules()
 });
+
+function renderSchedules() {
+    const html = `
+        <div class="card" style="padding: 24px;">
+            <div class="flex-row medium-bottom-margin">
+                <button class="base-button">Esta semana</button>
+                <button class="base-button">Próxima semana</button>
+            </div>
+        </div>
+    `;
+
+    document.getElementById('schedules').innerHTML = html;
+}
+
+renderSchedules()
