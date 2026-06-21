38761a57d8fb70c392d73235a80c10dfd13bdb18
joaopaulogui <joaopaulogmormota@gmail.com>
2026-06-21 03:07:55 -0300

    feat: added rooms control page

---
+++ js/rooms-control.js
@@ -0,0 +1,138 @@
+const floors = getFloors()
+
+function renderFloorResume(floor) {
+    const totalRooms = floor.rooms.length
+    var openRooms = 0;
+
+    floor.rooms.forEach(room => {
+        if (status == "unlocked") {openRooms++;}
+    });
+
+    const lockedRooms = totalRooms - openRooms;
+
+    return `
+        <div class="card" style="padding: 24px;">
+            <h3 class="bold big-text medium-bottom-margin">${floor.name}</h3>
+            <div class="flex-row" style="gap: 16px;">
+                <div class="flex-column" style="flex: 1;">
+                    <p class="light small-text smaller-bottom-margin">Total</p>
+                    <p class="bold large-text no-margin">${totalRooms}</p>
+                </div>
+                <div class="flex-column" style="flex: 1;">
+                    <p class="light small-text smaller-bottom-margin">Abertas</p>
+                    <p class="bold large-text green-text no-margin">${openRooms}</p>
+                </div>
+                <div class="flex-column" style="flex: 1;">
+                    <p class="light small-text smaller-bottom-margin">Trancadas</p>
+                    <p class="light bold large-text no-margin">${lockedRooms}</p>
+                </div>
+            </div>
+        </div>
+    `;
+}
+
+function getDoorIcon(status) {
+    if (status == "unlocked") {
+        return `
+            <svg class="logo-vec door-icon unlocked" viewBox="0 0 24 24">
+                <path d="M13 4h3a2 2 0 0 1 2 2v14"></path>
+                <path d="M2 20h3"></path>
+                <path d="M13 20h9"></path>
+                <path d="M10 12v.01"></path>
+                <path d="M13 4.562v16.157a1 1 0 0 1-1.242.97L5 20V5.562a2 2 0 0 1 1.515-1.94l4-1A2 2 0 0 1 13 4.561Z"></path>'
+            </svg>
+        `;
+    }
+
+    return `
+        <svg class="logo-vec door-icon locked" viewBox="0 0 24 24">
+            <path d="M18 20V6a2 2 0 0 0-2-2H8a2 2 0 0 0-2 2v14"></path>
+            <path d="M2 20h20"></path>
+            <path d<path d="M14 12v.01"></path>="M13 20h9"></path>
+        </svg>
+    `;
+}
+
+function getLockIcon(status) {
+    if (status == "unlocked") {
+        return `
+            <svg class="logo-vec lock-icon unlocked" viewBox="0 0 24 24">
+                <rect width="18" height="11" x="3" y="11" rx="2" ry="2"></rect>
+                <path d="M7 11V7a5 5 0 0 1 9.9-1"></path>
+            </svg>
+        `;
+    }
+
+    return `
+        <svg class="logo-vec lock-icon locked" viewBox="0 0 24 24">
+            <rect width="18" height="11" x="3" y="11" rx="2" ry="2"></rect>
+            <path d="M7 11V7a5 5 0 0 1 10 0v4"></path>
+        </svg>
+    `;
+}
+
+function renderTableRow(floorName, room) {
+    return `
+        <tr class="gray-border-bottom">
+            <td class="rooms-table-td">
+                <div class="flex-row" style="gap: 12px;">
+                    ${getDoorIcon(room.status)}
+                    <span class="bold small-text">${room.name}</span>
+                    </div>
+                    </td>
+                    <td class="small-text rooms-table-td">${floorName}</td>
+                    <td class="small-text rooms-table-td">${room.type}</td>
+                    <td class="small-text rooms-table-td">${room.capacity}</td>
+                    <td class="small-text rooms-table-td">
+                    <div class="flex-row ${room.status}-status-label">
+                    ${getLockIcon(room.status)}
+                    <span class="bold smaller-text">${room.status == "unlocked" ? "Destrancada" : "Trancada"}</span>
+                </div
+            </td>
+            <td class="rooms-table-td"><button class="base-button">${room.status == "unlocked" ? "Trancar" : "Destrancar"}</button></td>
+        </tr>
+    `;
+}
+
+function renderFloorsResume() {
+    const floorsResumeHtml = floors.map(floor => renderFloorResume(floor)).join('');
+
+    const html = `
+        <div class="grid cards big-bottom-margin">
+            ${floorsResumeHtml}
+        </div>
+    `;
+
+    document.getElementById('floors-resume').innerHTML = html;
+}
+
+function renderRoomsTable() {
+    const tableRowsHtml = floors.map(floor => 
+        floor.rooms.map(room => renderTableRow(floor.name, room)).join('')
+    ).join('');
+
+    const html = `
+        <div class="card">
+            <table id="rooms-table">
+                <thead class="rooms-table-thead">
+                    <tr>
+                        <th class="rooms-table-th light bold smaller-text">Sala</th>
+                        <th class="rooms-table-th light bold smaller-text">Andar</th>
+                        <th class="rooms-table-th light bold smaller-text">Tipo</th>
+                        <th class="rooms-table-th light bold smaller-text">Capacidade</th>
+                        <th class="rooms-table-th light bold smaller-text">Status</th>
+                        <th class="rooms-table-th light bold smaller-text">Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    ${tableRowsHtml}
+                </tbody>
+            </table>
+        </div>
+    `;
+
+    document.getElementById('rooms-table').innerHTML = html;
+}
+
+renderRoomsTable()
+renderFloorsResume()
