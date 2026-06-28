ff2de939a977f028863cd699abc65db2df608aea
TallesMoura <tallesmoura@alu.ufc.br>
2026-06-28 11:17:17 -0300

    feat: substitui status do AC por seletor direto (working/warning/broken)

--- pages/air-conditioners/actions.js
+++ pages/air-conditioners/actions.js
@@ -1,41 +1,59 @@
 function toggleAc(roomRame) {
-    for (const floor of floors) {
-        const room = floor.rooms.find(item => item.name === roomRame);
-        if (room) {
-            room.ac.isOn = !room.ac.isOn;
-            break;
-        }
+  for (const floor of floors) {
+    const room = floor.rooms.find((item) => item.name === roomRame);
+    if (room) {
+      room.ac.isOn = !room.ac.isOn;
+      break;
     }
+  }
 
-    saveFloors(allFloors);
-    renderAcsResume();
-    renderAcCards();
+  saveFloors(allFloors);
+  renderAcsResume();
+  renderAcCards();
 }
 
 function decreaseTemp(roomRame) {
-    for (const floor of floors) {
-        const room = floor.rooms.find(item => item.name === roomRame);
-        if (room) {
-            room.ac.temperature--;
-            break;
-        }
+  for (const floor of floors) {
+    const room = floor.rooms.find((item) => item.name === roomRame);
+    if (room) {
+      if (room.ac.temperature > 16) {
+        room.ac.temperature--;
+        break;
+      }
     }
+  }
 
-    saveFloors(allFloors);
-    renderAcsResume();
-    renderAcCards();
+  saveFloors(allFloors);
+  renderAcsResume();
+  renderAcCards();
 }
 
 function increaseTemp(roomRame) {
-    for (const floor of floors) {
-        const room = floor.rooms.find(item => item.name === roomRame);
-        if (room) {
-            room.ac.temperature++;
-            break;
-        }
+  for (const floor of floors) {
+    const room = floor.rooms.find((item) => item.name === roomRame);
+    if (room) {
+      if (room.ac.temperature < 32) {
+        room.ac.temperature++;
+        break;
+      }
     }
+  }
 
-    saveFloors(allFloors);
-    renderAcsResume();
-    renderAcCards();
+  saveFloors(allFloors);
+  renderAcsResume();
+  renderAcCards();
+}
+
+function changeAcStatus(roomName, newStatus) {
+  for (const floor of floors) {
+    const room = floor.rooms.find((item) => item.name === roomName);
+    if (room) {
+      room.ac.class = newStatus;
+      break;
+    }
+  }
+
+  saveFloors(allFloors);
+  renderAcsResume();
+  renderAcCards();
 }
--- pages/air-conditioners/templates.js
+++ pages/air-conditioners/templates.js
@@ -1,26 +1,28 @@
 function renderResume(floorName, rooms) {
-    const totalRooms = rooms.length;
-    let activeAcs = 0;
-    let workingAcs = 0;
-    let defectiveAcs = 0;
-    let brokenAcs = 0;
+  const totalRooms = rooms.length;
+  let activeAcs = 0;
+  let workingAcs = 0;
+  let defectiveAcs = 0;
+  let brokenAcs = 0;
 
-    rooms.forEach(room => {
-        if (room.ac.isOn) { activeAcs++; }
-        switch(room.ac.class) {
-            case "working":
-                workingAcs++;
-                break;
-            case "warning":
-                defectiveAcs++;
-                break;
-            case "broken":
-                brokenAcs++;
-                break;
-        }
-    });
+  rooms.forEach((room) => {
+    if (room.ac.isOn) {
+      activeAcs++;
+    }
+    switch (room.ac.class) {
+      case "working":
+        workingAcs++;
+        break;
+      case "warning":
+        defectiveAcs++;
+        break;
+      case "broken":
+        brokenAcs++;
+        break;
+    }
+  });
 
-    return `
+  return `
         <div class="card" style="padding: 24px;">
             <h3 class="bold big-text medium-bottom-margin">${floorName}</h3>
             <div class="flex-row gray-border-bottom small-bottom-margin" style="padding: 0px 0px 24px 0px;">
@@ -58,47 +60,50 @@ function renderResume(floorName, rooms) {
 }
 
 function chooseIcon(acClass) {
-    switch(acClass) {
-        case "working":
-            return `<svg class="logo-vec working ac-icons-vec" viewBox="0 0 24 24">${getIcon("checkMark")}</svg>`;
-        case "warning":
-            return `<svg class="logo-vec warning ac-icons-vec" viewBox="0 0 24 24">${getIcon("warning")}</svg>`;
-        case "broken":
-            return `<svg class="logo-vec broken ac-icons-vec" viewBox="0 0 24 24">${getIcon("x")}</svg>`;
-    }
+  switch (acClass) {
+    case "working":
+      return `<svg class="logo-vec working ac-icons-vec" viewBox="0 0 24 24">${getIcon("checkMark")}</svg>`;
+    case "warning":
+      return `<svg class="logo-vec warning ac-icons-vec" viewBox="0 0 24 24">${getIcon("warning")}</svg>`;
+    case "broken":
+      return `<svg class="logo-vec broken ac-icons-vec" viewBox="0 0 24 24">${getIcon("x")}</svg>`;
+  }
 }
 
 function chooseLabel(acClass) {
-    switch(acClass) {
-        case "working":
-            return "Funcionando"
-        case "warning":
-            return "Com Defeito"
-        case "broken":
-            return "Quebrado"
-    }
+  switch (acClass) {
+    case "working":
+      return "Funcionando";
+    case "warning":
+      return "Com Defeito";
+    case "broken":
+      return "Quebrado";
+  }
 }
 
 function renderButtons(item) {
-    if(item.ac.isOn) {
-        return `<button class="bold text base-button on-off-button on flex-row small-bottom-margin" data-toggle-ac="${item.name}">
+  if (item.ac.isOn) {
+    return `<button class="bold text base-button on-off-button on flex-row small-bottom-margin" data-toggle-ac="${item.name}">
             <svg class="logo-vec ac-on-off-vec" viewBox="0 0 24 24">${getIcon("power")}</svg>
             Desligar
         </button>
         <div class="flex-row" style="justify-content: space-between; gap: 8px;">
             <button class="base-button bold small-text temp-control-button" data-decrease-temp="${item.name}">- 1°C</button>
             <button class="base-button bold small-text temp-control-button" data-increase-temp="${item.name}">+ 1°C</button>
-        </div>`
-    } else {
-        return `<button class="bold text base-button on-off-button off flex-row"  data-toggle-ac="${item.name}">
+        </div>`;
+  } else {
+    return `<button class="bold text base-button on-off-button off flex-row"  data-toggle-ac="${item.name}">
             <svg class="logo-vec ac-on-off-vec" viewBox="0 0 24 24">${getIcon("power")}</svg>
             Ligar
-        </button>`
-    }
+        </button>`;
+  }
 }
 
 function renderCard(floorName, rooms) {
-    const cardsHtml = rooms.map(item => item.name.toLowerCase().includes(search.toLowerCase()) ? `
+  const cardsHtml = rooms
+    .map((item) =>
+      item.name.toLowerCase().includes(search.toLowerCase())
+        ? `
         <div class="card ac-card ${item.ac.class}" style="padding: 24px;">
             <div class="flex-row medium-bottom-margin" style="justify-content: space-between;">
                 <div>
@@ -109,12 +114,19 @@ function renderCard(floorName, rooms) {
                     <svg class="logo-vec ac-logo-vec" viewBox="0 0 24 24">${getIcon("air")}</svg>
                 </div>
             </div>
-            <div class="label flex-row ac-status ${item.ac.class}-label medium-bottom-margin" style="padding: 12px; justify-content: space-between;">
-                <div class="label flex-row" style="gap: 8px; align-items: center;">
-                    ${chooseIcon(item.ac.class)}
-                    <span class="bold small-text ${item.ac.class}">${chooseLabel(item.ac.class)}</span>
+            <div class="status-container medium-bottom-margin">
+                <div class="label flex-row ac-status ${item.ac.class}-label" style="padding: 12px; justify-content: space-between;">
+                    <div class="label flex-row" style="gap: 8px; align-items: center;">
+                        ${chooseIcon(item.ac.class)}
+                        <span class="bold small-text ${item.ac.class}">${chooseLabel(item.ac.class)}</span>
+                    </div>
+                    <svg class="logo-vec ac-config-vec" viewBox="0 0 24 24">${getIcon("cog")}</svg>
                 </div>
-                <svg class="logo-vec ac-config-vec" viewBox="0 0 24 24">${getIcon("cog")}</svg>
+                <select class="status-select-overlay" data-change-status="${item.name}">
+                    <option value="working" ${item.ac.class === "working" ? "selected" : ""}>Funcionando</option>
+                    <option value="warning" ${item.ac.class === "warning" ? "selected" : ""}>Com Defeito</option>
+                    <option value="broken" ${item.ac.class === "broken" ? "selected" : ""}>Quebrado</option>
+                </select>
             </div>
             <div class="flex-row small-bottom-margin" style="gap: 8px; align-items: center;">
                 <svg class="logo-vec ac-config-vec" viewBox="0 0 24 24">${getIcon("temperature")}</svg>
@@ -126,10 +138,13 @@ function renderCard(floorName, rooms) {
             </div>
             ${renderButtons(item)}
         </div>
-    ` : '').join('');
+    `
+        : "",
+    )
+    .join("");
 
-    return `
+  return `
         <h3 class="bold big-text medium-bottom-margin gray-border-bottom" style="padding: 0px 0px 8px 0px;">${floorName}</h3>
         <div class="grid cards bigger-bottom-margin" style="grid-template-columns: repeat(3, minmax(212px, 1fr));">${cardsHtml}</div>
-    `
+    `;
 }
