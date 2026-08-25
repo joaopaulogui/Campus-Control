b009f0afc3d26ac5f9b4cac13cd61a2b422025b4
joaopaulogui <joaopaulogmormota@gmail.com>
2026-08-25 10:24:25 -0300

    feat: implement schedule class

--- back-end/src/entities/schedule.ts
+++ back-end/src/entities/schedule.ts
@@ -6,3 +6,55 @@ export interface ScheduleProps {
     endHour: number
     title: string
 }
+
+export class Schedule {
+    private props: ScheduleProps
+
+    constructor(props: ScheduleProps) {
+        this.props = props
+    }
+
+    get id() {
+        return this.props.id
+    }
+
+    get roomId() {
+        return this.props.roomId
+    }
+    
+    set roomId(roomId: string) {
+        this.props.roomId = roomId
+    }
+
+    get date() {
+        return this.props.date
+    }
+
+    set date(date: Date) {
+        this.props.date = date
+    }
+
+    get startHour() {
+        return this.props.startHour
+    }
+
+    set startHour(startHour: number) {
+        this.props.startHour = startHour
+    }
+
+    get endHour() {
+        return this.props.endHour
+    }
+
+    set endHour(endHour: number) {
+        this.props.endHour = endHour
+    }
+
+    get title() {
+        return this.props.title
+    }
+
+    set title(title: string) {
+        this.props.title = title
+    }
+}
