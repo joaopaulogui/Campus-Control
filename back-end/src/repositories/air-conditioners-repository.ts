11d9ec3cb12d79d59108bb61cc3b8f0c8209c69e
joaopaulogui <joaopaulogmormota@gmail.com>
2026-08-25 21:42:12 -0300

    refac: change method in air conditioners repository

--- back-end/src/repositories/air-conditioners-repository.ts
+++ back-end/src/repositories/air-conditioners-repository.ts
@@ -3,8 +3,7 @@ import { AirConditioner } from "../entities/air-conditioner";
 export interface AirConditionersRepository {
     create(airConditioner: AirConditioner): Promise<void>
     findById(id: string): Promise<AirConditioner | null>
-    findManyByRoomId(roomId: string): Promise<AirConditioner[]>
-    findManyByFloorId(floorId: string): Promise<AirConditioner[]>
+    findMany(floorId?: string): Promise<AirConditioner[]>
     save(airConditioner: AirConditioner): Promise<void>
     delete(airConditioner: AirConditioner): Promise<void>
 }
