c709cb082144262eb9daedf815d8edad87a1d61a
joaopaulogui <joaopaulogmormota@gmail.com>
2026-08-27 00:42:44 -0300

    fix: fix db connection

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,9 +1,22 @@
 import express from 'express'
+import { prisma } from './lib/prisma';
 
 const app = express()
 app.use(express.json())
 
+
+async function main() {
+  const users = await prisma.user.findMany();
+  console.log("Conexão OK! Usuários encontrados:", users);
+}
+
+
+
 app.get('/', (req, res) => {
+    main()
+        .catch((e) => console.error("Erro:", e))
+        .finally(() => prisma.$disconnect());
+
     res.json({ ok: true })
 })
 
