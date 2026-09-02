7f724ed703f5fdc16636258411b0d38e696ff41c
joaopaulogui <joaopaulogmormota@gmail.com>
2026-09-02 12:24:43 -0300

    feat: finish list floor rooms use case

--- back-end/src/use-cases/list-floor-rooms-use-case.ts
+++ back-end/src/use-cases/list-floor-rooms-use-case.ts
@@ -1,20 +1,39 @@
+import type { Floor } from "../entities/floor.js";
 import type { Room } from "../entities/room.js";
+import type { FloorsRepository } from "../repositories/floors-repository.js";
 import type { RoomsRepository } from "../repositories/rooms-repository.js";
 
-interface ListFloorRoomsUseCaseRequest {
-    floorId: string
-}
-
-interface ListFloorRoomsUseCaseResponse {
+interface FloorWithRooms {
+    floor: Floor
     rooms: Room[]
 }
 
+interface ListFloorRoomsUseCaseRequest {
+    floorId?: string
+}
+
+interface ListFloorRoomsUseCaseResponse {
+    floors: FloorWithRooms[]
+}
+
 export class ListFloorRoomsUseCase {
-    constructor(private roomsRepository: RoomsRepository) {}
+    constructor(
+        private floorsRepository: FloorsRepository,
+        private roomsRepository: RoomsRepository,
+    ) {}
 
     async execute({ floorId }: ListFloorRoomsUseCaseRequest): Promise<ListFloorRoomsUseCaseResponse> {
-        const rooms = await this.roomsRepository.findManyByFloorId(floorId)
+        const floors = floorId
+            ? await this.floorsRepository.findMany({ id: floorId })
+            : await this.floorsRepository.findMany()
+        
+        const rooms = await this.roomsRepository.findMany({ floorId, })
 
-        return { rooms, }
+        const floorsWithRooms = floors.map(floor => ({
+            floor,
+            rooms: rooms.filter(room => room.floorId === floor.id),
+        }))
+
+        return { floors: floorsWithRooms, }
     }
 }
