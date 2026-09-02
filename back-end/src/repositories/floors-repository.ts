181f76f43b63e1cdb8a5398fa0c8c1c8d0f188b3
joaopaulogui <joaopaulogmormota@gmail.com>
2026-09-02 12:12:53 -0300

    feat: update floors and rooms repository to allow filtered findMany

--- back-end/src/repositories/floors-repository.ts
+++ back-end/src/repositories/floors-repository.ts
@@ -1,7 +1,12 @@
 import { Floor } from "../entities/floor.js"
 
+export interface FloorFilters {
+    id?: string
+}
+
 export interface FloorsRepository {
     create(floor: Floor): Promise<void>
     save(floor: Floor): Promise<void>
+    findMany(filters?: FloorFilters): Promise<Floor[]>
     delete(floor: Floor): Promise<void>
 }
