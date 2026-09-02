9e03bb16e45b7df24c0927800296937b1db36dc8
joaopaulogui <joaopaulogmormota@gmail.com>
2026-09-02 20:36:32 -0300

    feat: add create room and create floor controllers and routes

--- back-end/src/routes/floors.ts
+++ back-end/src/routes/floors.ts
@@ -1,10 +1,18 @@
 import express from "express"
 import { ListFloorRoomsController } from "../controllers/list-floor-rooms-controller"
+import { CreateFloorController } from "../controllers/create-floor-controller"
+import { CreateRoomController } from "../controllers/create-room-controller"
 
 const router = express.Router()
 
+const createFloorController = new CreateFloorController()
+const createRoomController = new CreateRoomController()
 const listFloorRoomsController = new ListFloorRoomsController()
 
 router.get('/', (req, res) => listFloorRoomsController.handle(req, res))
+
+router.post('/', (req, res) => createFloorController.handle(req, res))
+
+router.post('/:floorId/rooms', (req, res) => createRoomController.handle(req, res))
 
 export default router
