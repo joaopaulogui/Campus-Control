8d04452abb8ec783b267ecfcda57521221d06200
joaopaulogui <joaopaulogmormota@gmail.com>
2026-09-02 21:10:24 -0300

    fix: add cors config

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,7 +1,11 @@
 import express from 'express'
+import cors from 'cors'
+
 import floorRoutes from './routes/floors'
 
 const app = express()
+
+app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
