23901fb0b4f288068b562c56fe165eb0ef07f8c8
joaopaulogui <joaopaulogmormota@gmail.com>
2026-09-03 11:45:52 -0300

    feat: add toggle room lock controller and route

--- back-end/src/routes/rooms.ts
+++ back-end/src/routes/rooms.ts
@@ -1,14 +1,18 @@
 import express from "express"
 import { CreateRoomController } from "../controllers/create-room-controller"
 import { ListFloorRoomsController } from "../controllers/list-floor-rooms-controller"
+import { ToggleRoomLockController } from "../controllers/toggle-room-lock-controller"
 
 const router = express.Router()
 
 const createRoomController = new CreateRoomController()
 const listFloorRoomsController = new ListFloorRoomsController()
+const toggleRoomLockController = new ToggleRoomLockController()
 
 router.post('/', (req, res) => createRoomController.handle(req, res))
 
 router.get('/', (req, res) => listFloorRoomsController.handle(req, res))
+
+router.patch('/:roomId/lock', (req, res) => toggleRoomLockController.handle(req, res))
 
 export default router
