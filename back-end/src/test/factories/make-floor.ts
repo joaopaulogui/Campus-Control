49db6c281a657dcc61aeec42295f40aa630d3eaa
joaopaulogui <joaopaulogmormota@gmail.com>
2026-09-06 19:34:13 -0300

    test: add buildingId property

--- back-end/src/test/factories/make-floor.ts
+++ back-end/src/test/factories/make-floor.ts
@@ -1,9 +1,11 @@
+import { randomUUID } from "node:crypto";
 import { Floor, type FloorProps } from "../../entities/floor";
 import { faker } from "@faker-js/faker"
 
 export function makeFloor(override: Partial<FloorProps> = {}, id?: string): Floor {
     const floor = new Floor({
         name: faker.lorem.word(),
+        buildingId: randomUUID(),
         ...override,
     }, id)
 
