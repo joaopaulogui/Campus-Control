6c71181d25137d09cdb700a4515d8e91ee2d1f76
joaopaulogui <joaopaulogmormota@gmail.com>
2026-09-06 19:32:12 -0300

    feat: add filter by name

--- back-end/src/repositories/items-repository.ts
+++ back-end/src/repositories/items-repository.ts
@@ -1,9 +1,13 @@
 import { Item } from "../entities/item.js";
 
+export interface ItemFilter {
+    name?: string | undefined
+}
+
 export interface ItemsRepository {
     create(item: Item): Promise<void>
     findById(id: string): Promise<Item | null>
-    findByName(name: string): Promise<Item | null> 
+    findMany(filters?: ItemFilter): Promise<Item[]>
     save(item: Item): Promise<void>
     delete(item: Item): Promise<void>
 }
