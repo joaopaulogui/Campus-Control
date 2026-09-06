e0eb8eca81a3b3e0cb3a0bfd1067f672075911e5
joaopaulogui <joaopaulogmormota@gmail.com>
2026-09-06 19:22:03 -0300

    feat: add property buildingId and updatedAt

--- back-end/src/entities/floor.ts
+++ back-end/src/entities/floor.ts
@@ -2,6 +2,8 @@ import { randomUUID } from "node:crypto"
 
 export interface FloorProps {
     name: string 
+    buildingId: string
+    updatedAt?: Date | null
 }
 
 export class Floor {
@@ -23,5 +25,22 @@ export class Floor {
 
     set name(name: string) {
         this.props.name = name
+        this.touch()
+    }
+
+    get buildingId() {
+        return this.props.buildingId
+    }
+
+    set buildingId(buildingId: string) {
+        this.props.buildingId = buildingId
+    }
+
+    get updatedAt() {
+        return this.props.updatedAt
+    }
+    
+    private touch() {
+        this.props.updatedAt = new Date()
     }
 }
