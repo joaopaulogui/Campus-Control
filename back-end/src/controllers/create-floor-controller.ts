90b854a0175a4866fdfdd12febf3647cb147fa27
joaopaulogui <joaopaulogmormota@gmail.com>
2026-09-06 21:00:41 -0300

    fix: add building id to body schema

--- back-end/src/controllers/create-floor-controller.ts
+++ back-end/src/controllers/create-floor-controller.ts
@@ -2,20 +2,23 @@ import { type Request, type Response } from 'express'
 import { z } from "zod";
 import { PrismaFloorsRepository } from '../repositories/prisma/prisma-floors-repository';
 import { CreateFloorUseCase } from '../use-cases/create-floor-use-case';
+import { PrismaBuildingsRepository } from '../repositories/prisma/prisma-buildings-repository';
 
 const CreateFloorBodySchema = z.object({
-    name: z.string()
+    name: z.string(),
+    buildingId: z.uuid(),
 })
 
 export class CreateFloorController {
     async handle(req: Request, res: Response) {
         const floorsRepository = new PrismaFloorsRepository()
+        const buildingsRepository = new PrismaBuildingsRepository()
 
-        const createFloor = new CreateFloorUseCase(floorsRepository)
+        const createFloor = new CreateFloorUseCase(floorsRepository, buildingsRepository)
 
-        const { name } = CreateFloorBodySchema.parse(req.body)
+        const { name, buildingId } = CreateFloorBodySchema.parse(req.body)
 
-        await createFloor.execute({ name })
+        await createFloor.execute({ name, buildingId })
 
         res.status(201).send()
     }
