e15037f5bf44b110d2ed8b72e41b150678dabcbb
joaopaulogui <joaopaulogmormota@gmail.com>
2026-09-07 00:46:41 -0300

    feat: add createdAt and updatedAt getters

--- back-end/src/entities/user.ts
+++ back-end/src/entities/user.ts
@@ -63,6 +63,14 @@ export class User {
         this.touch()
     }
 
+    get createdAt() {
+        return this.props.createdAt
+    }
+
+    get updatedAt() {
+        return this.props.updatedAt
+    }
+
     private touch() {
         this.props.updatedAt = new Date()
     }
