6c7d549aa0dd58cdf4249a0e8cd6df1f6739aa5d
joaopaulogui <joaopaulogmormota@gmail.com>
2026-09-09 20:39:01 -0300

    chore: add start value to override

--- back-end/src/test/factories/make-air-conditioner.ts
+++ back-end/src/test/factories/make-air-conditioner.ts
@@ -2,7 +2,7 @@ import { faker } from "@faker-js/faker";
 import { AirConditioner, AirConditionerStatus, type AirConditionerProps } from "../../entities/air-conditioner";
 import { randomUUID } from "node:crypto";
 
-export function makeAirConditioner(override: Partial<AirConditionerProps>, id?: string): AirConditioner {
+export function makeAirConditioner(override: Partial<AirConditionerProps> = {}, id?: string): AirConditioner {
     const airConditioner = new AirConditioner({
         roomId: randomUUID(),
         temperature: faker.number.int({ min: 15, max: 35 }),
