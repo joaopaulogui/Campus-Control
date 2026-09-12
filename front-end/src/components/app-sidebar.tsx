1637e382d2c29e03064a0f2709f3a78f13c703fd
Marcos Gabriel <marcosggomes27@gmail.com>
2026-09-12 18:26:31 -0300

    feat: create sidebar structure

--- front-end/src/components/app-sidebar.tsx
+++ front-end/src/components/app-sidebar.tsx
@@ -4,17 +4,99 @@ import {
   SidebarFooter,
   SidebarGroup,
   SidebarHeader,
+  SidebarMenu,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+  useSidebar,
 } from "@/components/ui/sidebar"
+import { BuildingComplex, House, Wind, DoorClosed, Package } from "lucide-react"
+import { Link, useLocation, useNavigate } from "react-router-dom"
+
+const menu = [
+  {
+    title: "Menu",
+    icon: null,
+    collapsed: true,
+    items: [
+      {
+        title: "Dashboard",
+        url: "/",
+        icon: House,
+        nivel_acesso: ["Administrador", "Funcionario"]
+      },
+      {
+        title: "Ar-Condicionado",
+        url: "/ar-condicionado",
+        icon: Wind,
+        nivel_acesso: ["Administrador", "Funcionario"]
+      },
+      {
+        title: "Controle de Salas",
+        url: "/controle-de-salas",
+        icon: DoorClosed,
+        nivel_acesso: ["Administrador", "Funcionario"]
+      },
+      {
+        title: "Empréstimos",
+        url: "/emprestimos",
+        icon: Package,
+        nivel_acesso: ["Administrador", "Funcionario"]
+      },
+    ]
+  }
+]
 
 export function AppSidebar() {
+
+  const navigate = useNavigate()
+  //const { user, setUser, setToken } = useAuth()
+  const { isMobile } = useSidebar()
+  const { pathname } = useLocation()
+
+  function isActive(url: string) {
+    return url.split("/")[1] === pathname.split("/")[1]
+  }
+
   return (
     <Sidebar>
-      <SidebarHeader />
+      <SidebarHeader>
+        <div className="flex flex-row gap-3 items-center my-4 mx-2">
+          <div className="h-12 w-12 shrink-0 rounded-md bg-sidebar-accent flex items-center justify-center">
+            <BuildingComplex className="text-white h-7 w-7" />
+          </div>
+          <div>
+            <p className="text-xl font-semibold text-sidebar-primary-foreground">Campus Control</p>
+            <p className="text-xs text-sidebar-accent-foreground">Sistema Operacional</p>
+          </div>
+        </div>
+      </SidebarHeader>
+      <hr className="border border-sidebar-border" />
       <SidebarContent>
-        <SidebarGroup />
-        <SidebarGroup />
-      </SidebarContent>
+        <SidebarGroup>
+          <SidebarMenu className="gap-3 my-3">
+            {menu.map((item) => (
+              item.items?.map((subItem) => {
+                const Icon = subItem.icon
+                return (
+                  //subItem.nivel_acesso.includes(user?.tipo_acesso || "") ? 
+                  <SidebarMenuSubItem key={subItem.title}>
+                    <SidebarMenuSubButton
+                      className={`rounded-sm text-sidebar-accent-foreground hover:text-white hover:bg-sidebar-accent/70 p-6 mx-2 ${isActive(subItem.url) ? "text-white bg-sidebar-accent" : ""}`}>
+                      <Link to={subItem.url} className="flex flex-row gap-2">
+                        <Icon />
+                        <span className="font-semibold text-[16px]">{subItem.title}</span>
+                      </Link>
+                    </SidebarMenuSubButton>
+                  </SidebarMenuSubItem>
+                  // : <></>
+                )
+              })
+            ))
+            }
+          </SidebarMenu >
+        </SidebarGroup >
+      </SidebarContent >
       <SidebarFooter />
-    </Sidebar>
+    </Sidebar >
   )
 }
--- front-end/src/components/layout-page.tsx
+++ front-end/src/components/layout-page.tsx
@@ -14,13 +14,9 @@ export default function LayoutPage() {
         <SidebarProvider>
             <AppSidebar />
             <SidebarInset className="min-w-0">
-                <header className="relative flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+                <header className="relative border-b border-sidebar-border flex h-[72px] shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
                     <div className="flex items-center gap-2 px-4">
                         <SidebarTrigger className="-ml-1" />
-                        <Separator
-                            orientation="vertical"
-                            className="mr-2 data-[orientation=vertical]:h-4"
-                        />
                         <div className="absolute top-2 right-2">
                             <ModeToggle />
                         </div>
