69392fa42a6cddf9d34a343ceff1ae109aab4841
joaopaulogui <joaopaulogmormota@gmail.com>
2026-09-20 13:39:26 -0300

    feat: add register item use case

---
+++ back-end/src/use-cases/register-item-use-case.ts
@@ -0,0 +1,28 @@
+import { Item, type ItemType } from "../entities/item"
+import type { ItemsRepository } from "../repositories/items-repository"
+
+interface RegisterItemUseCaseRequest {
+    name: string
+    type: ItemType
+    totalQuantity: number
+}
+
+interface RegisterItemUseCaseResponse {}
+
+export class RegisterItemUseCase {
+    constructor(private itemsRepository: ItemsRepository) {}
+
+    async execute({ name, type, totalQuantity }: RegisterItemUseCaseRequest): Promise<RegisterItemUseCaseResponse> {
+        const item = new Item({
+            name,
+            type,
+            totalQuantity,
+            availableQuantity: 0,
+            onHoldQuantity: 0,
+        })
+
+        await this.itemsRepository.create(item)
+
+        return {}
+    }
+}
