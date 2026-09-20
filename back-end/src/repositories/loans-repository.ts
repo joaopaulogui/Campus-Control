57901bf5dea8388b7b34b7cfb72fd362ef6bb4bb
joaopaulogui <joaopaulogmormota@gmail.com>
2026-09-20 15:38:47 -0300

    refac: join findMany functions in a single function

--- back-end/src/repositories/loans-repository.ts
+++ back-end/src/repositories/loans-repository.ts
@@ -1,10 +1,14 @@
-import { Item } from "../entities/item.js";
-import { Loan } from "../entities/loan.js";
+import { Loan, LoanStatus } from "../entities/loan.js";
+
+export interface LoanFilters {
+    responsibleName?: string | undefined,
+    itemIds?: string[] | undefined,
+    status?: LoanStatus | undefined,
+}
 
 export interface LoansRepository {
     create(loan: Loan): Promise<void>
-    findManyByResponsibleName(ResponsibleName: string): Promise<Item[]>
-    findManyByItemName(itemName: string): Promise<Item[]>
+    findMany(filters?: LoanFilters): Promise<Loan[]>
     save(loan: Loan): Promise<void>
     delete(loan: Loan): Promise<void>
 }
