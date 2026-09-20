d6c95e29ebb1c3011442769b14a544f84514ca55
joaopaulogui <joaopaulogmormota@gmail.com>
2026-09-20 15:39:19 -0300

    quickfix: change number to integer

--- back-end/src/http/schemas/items.ts
+++ back-end/src/http/schemas/items.ts
@@ -4,5 +4,5 @@ import { ItemType } from "../../entities/item"
 export const RegisterItemBodySchema = z.object({
     name: z.string(),
     type: z.enum(ItemType),
-    totalQuantity: z.number()
+    totalQuantity: z.int()
 })
